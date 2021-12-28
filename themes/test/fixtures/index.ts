aeb3dc4e57485937378d20273b581a260135d63b
andrew.walpole <andrew.walpole@nbrown.co.uk>
2021-12-28 10:57:16 +0000

    added new theme files

--- themes/test/fixtures/index.ts
+++ themes/test/fixtures/index.ts
@@ -1,5 +1,5 @@
-export { default as listingQuery } from "./listingQuery";
-export { default as postQuery } from "./postQuery";
-export { default as post } from "./post";
-export { default as postListing } from "./postListing";
-export { default as config } from "./config";
+export { default as listingQuery } from './listingQuery'
+export { default as postQuery } from './postQuery'
+export { default as post } from './post'
+export { default as postListing } from './postListing'
+export { default as config } from './config'
